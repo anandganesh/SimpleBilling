4ebaff3b9e3c94acf2ddc71262e2bb5cc43120f1
ANANDGANESH S S <ANANDGANESH S S>
2016-02-14 22:53:16 +0530

    Export to Xls

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
-
-$(document).ready(function(){
-    var rate,qty,price,P_name;
-    $(".bill_no").text(Math.floor((Math.random() * 1000) + 1));
-    $(".date").text(new Date());
-    $("#rate").blur(function(){
-        qty=$("#qty").val();
-        rate = $("#rate").val();
-        P_name=$("#P_name").val();
-    if((qty!="")&& rate!="")
-    {
-        price =qty*rate;
-        $("#price").val(price);
-    }
-    });
-    $("#add").click(function(){
-        if(P_name!=undefined || qty!=undefined || rate!=undefined||price!=undefined  ){
-        var list_item = "$((<tr><td>"+P_name+"</td><td>"+qty+"</td><td>"+rate+"</td><td>"+price+"</td>";
-        $(".addValue").append(list_item);
-            $("#qty").val("");
-            $("#rate").val("");
-            $("#P_name").val("");
-            $("#price").val("");
-        }
-    });
-    function print(){
-        window.print();
-    }
-});
+$(document).ready(
+		function() {
+			var rate, qty, price, P_name;
+			$(".bill_no").text(Math.floor((Math.random() * 1000) + 1));
+			$(".date").text(new Date());
+			$("#rate").blur(function() {
+				qty = $("#qty").val();
+				rate = $("#rate").val();
+				P_name = $("#P_name").val();
+				if ((qty != "") && rate != "") {
+					price = qty * rate;
+					$("#price").val(price);
+				}
+			});
+			$("#add").click(
+					function() {
+						if (P_name != undefined || qty != undefined
+								|| rate != undefined || price != undefined) {
+							var list_item = "$((<tr><td>" + P_name
+									+ "</td><td>" + qty + "</td><td>" + rate
+									+ "</td><td>" + price + "</td>";
+							$(".addValue").append(list_item);
+							$("#qty").val("");
+							$("#rate").val("");
+							$("#P_name").val("");
+							$("#price").val("");
+						}
+					});
+			$(".print").click(function() {
+				$(".AddInfo").hide();
+				$("h1").hide();
+				$(".print").hide();
+				tableToExcel('testTable', 'W3C Example Table');
+                window.print();
+			});
+			// function print1() {
+			// $(".AddInfo").hide();
+			// }
+		});
